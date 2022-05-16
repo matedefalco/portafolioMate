2f206b0f545184e75813ba44ef150f75a015eef2
Mateo De Falco <mateodefalco1@gmail.com>
2022-05-16 15:57:19 -0300

    probando commit

---
+++ backend.sanity/schemas/info.js
@@ -0,0 +1,23 @@
+export default{
+   name: 'info',
+   title: 'Info',
+   type: 'document',
+   fields:[
+      {
+         name: 'name',
+         tite: 'Name',
+         type: 'string'
+      },
+      {
+         name: 'subCategorias',
+         tite: 'Sub Categorías',
+         type: 'string'
+      },
+      {
+         name: 'url',
+         tite: 'urls',
+         type: 'string'
+      },
+
+   ],
+}
--- frontend_react/src/container/index.js
+++
@@ -1,6 +0,0 @@
-export { default as About } from './About/about';
-export { default as Footer } from './footer/footer';
-export { default as Header } from './header/header';
-export { default as Skills } from './skills/skills';
-export { default as Testimonial } from './testimonial/testimonial';
-export { default as Work } from './work/work';

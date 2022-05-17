cef114ad3eedac5516eb5521018cbec024dc7c66
Mateo De Falco <mateodefalco1@gmail.com>
2022-05-17 14:53:27 -0300

    CORRECIÓN navbar final

--- pagina_wiki/src/components/Navbar/Navbar.jsx
+++ pagina_wiki/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { useState, setToggle } from 'react';
+import React, { useState } from 'react';
+import { setToggle } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
 
 import  './Navbar.scss';
 import { Images } from '../../constants'
  const Navbar = () => {
-    const [toggle, settoggle] = useState(false)
+    const [toggle, setToggle] = useState(false)
    return (
       <nav className="app__navbar">
          <div className="app__navbar-logo">
@@ -24,18 +24,17 @@ import { Images } from '../../constants'
             <HiMenuAlt4 onClick={() => setToggle(true)}/>
             {toggle && (
                   <motion.div
-                     whileInView={{ x: [300, 0]}}
+                     whileInView={{ x: [300, 0] }}
                      transition={{ duration: 0.85, ease: 'easeOut'}}
                      >
                         <HiX onClick={() => setToggle(false)}/>
-                        <ul>
-                        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
-                        <li key={item}>
-                           <div />
-                           <a href={'#${item}'} onClick={() => setToggle(false)}>{item}</a>
-                        </li>
-                        ))}
-                        </ul>
+                           <ul>
+                           {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                              <li key={item}>
+                                 <a href={'#${item}'} onClick={() => setToggle(false)}>{item}</a>
+                              </li>
+                           ))}
+                           </ul>
                   </motion.div>
             )}
          </div>
